6b25cf1f82d8bb0db81120fdbfff8dc6d1eed450
AlPridan <prao@yandex.ru>
2019-06-22 21:55:26 +0300

    Тест для контактов

---
+++ addressbook-web-tests/addressbook-web-tests/ContactCreatonTests.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactCreatonTests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "http://localhost";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void ContactCreatonTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToNewContactPage();
+            ContactData group = new ContactData("Micky");
+            group.MiddleName = "Rat";
+            group.SecNotes = "some of the text";
+            group.BDay = "12";
+            group.BMonth = "September";
+            group.BYear = "1990";
+            group.LastName = "Mouse";
+            group.NickName = "MickyMouse";
+            group.Address = "2st newYork";
+            group.TelFax = "999";
+            group.Title = "RudeMouse";
+            group.Company = "Disney";
+            group.TelHome = "888";
+            group.TelWork = "777";
+            group.TelMobile = "666";
+            group.Email1 = "mm1@disney.com";
+            group.Email2 = "mm2@disney.com";
+            group.Email3 = "mm3@disney.com";
+            group.HomePage = "neverMore.com";
+            group.SecAddress = "Krizhopol 3st";
+            group.SecHome = "35 building";
+            group.ADay = "5";
+            group.AMonth = "May";
+            group.AYear = "1995";
+            FillContactForm(group);
+            SubmitContactCreation();
+        }
+
+        private void SubmitContactCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillContactForm(ContactData group)
+        {
+            driver.FindElement(By.Name("firstname")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(group.FirstName);
+            driver.FindElement(By.Name("middlename")).Clear();
+            driver.FindElement(By.Name("middlename")).SendKeys(group.MiddleName);
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(group.LastName);
+            driver.FindElement(By.Name("nickname")).Clear();
+            driver.FindElement(By.Name("nickname")).SendKeys(group.NickName);
+            driver.FindElement(By.Name("title")).Clear();
+            driver.FindElement(By.Name("title")).SendKeys(group.Title);
+            driver.FindElement(By.Name("company")).Clear();
+            driver.FindElement(By.Name("company")).SendKeys(group.Company);
+            driver.FindElement(By.Name("address")).Clear();
+            driver.FindElement(By.Name("address")).SendKeys(group.Address);
+            driver.FindElement(By.Name("home")).Clear();
+            driver.FindElement(By.Name("home")).SendKeys(group.TelHome);
+            driver.FindElement(By.Name("mobile")).Clear();
+            driver.FindElement(By.Name("mobile")).SendKeys(group.TelMobile);
+            driver.FindElement(By.Name("work")).Clear();
+            driver.FindElement(By.Name("work")).SendKeys(group.TelWork);
+            driver.FindElement(By.Name("fax")).Clear();
+            driver.FindElement(By.Name("fax")).SendKeys(group.TelFax);
+            driver.FindElement(By.Name("email")).Clear();
+            driver.FindElement(By.Name("email")).SendKeys(group.Email1);
+            driver.FindElement(By.Name("email2")).Clear();
+            driver.FindElement(By.Name("email2")).SendKeys(group.Email2);
+            driver.FindElement(By.Name("email3")).Clear();
+            driver.FindElement(By.Name("email3")).SendKeys(group.Email3);
+            driver.FindElement(By.Name("homepage")).Clear();
+            driver.FindElement(By.Name("homepage")).SendKeys(group.HomePage);
+            driver.FindElement(By.Name("bday")).Click();
+            new SelectElement(driver.FindElement(By.Name("bday"))).SelectByText(group.BDay);
+            driver.FindElement(By.Name("bday")).Click();
+            driver.FindElement(By.Name("bmonth")).Click();
+            new SelectElement(driver.FindElement(By.Name("bmonth"))).SelectByText(group.BMonth);
+            driver.FindElement(By.Name("bmonth")).Click();
+            driver.FindElement(By.Name("byear")).Click();
+            driver.FindElement(By.Name("byear")).Clear();
+            driver.FindElement(By.Name("byear")).SendKeys(group.BYear);
+            driver.FindElement(By.Name("aday")).Click();
+            new SelectElement(driver.FindElement(By.Name("aday"))).SelectByText(group.ADay);
+            driver.FindElement(By.Name("aday")).Click();
+            driver.FindElement(By.Name("amonth")).Click();
+            new SelectElement(driver.FindElement(By.Name("amonth"))).SelectByText(group.AMonth);
+            driver.FindElement(By.Name("amonth")).Click();
+            driver.FindElement(By.Name("ayear")).Click();
+            driver.FindElement(By.Name("ayear")).Clear();
+            driver.FindElement(By.Name("ayear")).SendKeys(group.AYear);
+            driver.FindElement(By.Name("address2")).Click();
+            driver.FindElement(By.Name("address2")).Clear();
+            driver.FindElement(By.Name("address2")).SendKeys(group.SecAddress);
+            driver.FindElement(By.Name("phone2")).Clear();
+            driver.FindElement(By.Name("phone2")).SendKeys(group.SecHome);
+            driver.FindElement(By.Name("notes")).Clear();
+            driver.FindElement(By.Name("notes")).SendKeys(group.SecNotes);
+        }
+
+        private void GoToNewContactPage()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.CssSelector("input[type =\"submit\"]")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/addressbook/");
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+        private string nickName;
+        private string title;
+        private string company;
+        private string address;
+        private string telHome;
+        private string telMobile;
+        private string telWork;
+        private string telFax;
+        private string email1;
+        private string email2;
+        private string email3;
+        private string homePage;
+        private string bDay;
+        private string bMonth;
+        private string bYear;
+        private string aDay;
+        private string aMonth;
+        private string aYear;
+        private string secAddress;
+        private string secHome;
+        private string secNotes;
+
+        public ContactData(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+          
+        public string MiddleName
+        {
+            get
+            {
+                return middleName;
+            }
+            set
+            {
+                middleName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string NickName
+        {
+            get
+            {
+                return nickName;
+            }
+            set
+            {
+                nickName = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Company
+        {
+            get
+            {
+                return company;
+            }
+            set
+            {
+                company = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+
+        public string TelHome
+        {
+            get
+            {
+                return telHome;
+            }
+            set
+            {
+                telHome = value;
+            }
+        }
+        public string TelMobile
+        {
+            get
+            {
+                return telMobile;
+            }
+            set
+            {
+                telMobile = value;
+            }
+        }
+        public string TelWork
+        {
+            get
+            {
+                return telWork;
+            }
+            set
+            {
+                telWork = value;
+            }
+        }
+        public string TelFax
+        {
+            get
+            {
+                return telFax;
+            }
+            set
+            {
+                telFax = value;
+            }
+        }
+        public string Email1
+        {
+            get
+            {
+                return email1;
+            }
+            set
+            {
+                email1 = value;
+            }
+        }
+        public string Email2
+        {
+            get
+            {
+                return email2;
+            }
+            set
+            {
+                email2 = value;
+            }
+        }
+        public string Email3
+        {
+            get
+            {
+                return email3;
+            }
+            set
+            {
+                email3 = value;
+            }
+        }
+        public string HomePage
+        {
+            get
+            {
+                return homePage;
+            }
+            set
+            {
+                homePage = value;
+            }
+        }
+        public string BDay
+        {
+            get
+            {
+                return bDay;
+            }
+            set
+            {
+                bDay = value;
+            }
+        }
+        public string BMonth
+        {
+            get
+            {
+                return bMonth;
+            }
+            set
+            {
+                bMonth = value;
+            }
+        }
+        public string BYear
+        {
+            get
+            {
+                return bYear;
+            }
+            set
+            {
+                bYear = value;
+            }
+        }
+        public string ADay
+        {
+            get
+            {
+                return aDay;
+            }
+            set
+            {
+                aDay = value;
+            }
+        }
+        public string AMonth
+        {
+            get
+            {
+                return aMonth;
+            }
+            set
+            {
+                aMonth = value;
+            }
+        }
+
+        public string AYear
+        {
+            get
+            {
+                return aYear;
+            }
+            set
+            {
+                aYear = value;
+            }
+        }
+
+        public string SecAddress
+        {
+            get
+            {
+                return secAddress;
+            }
+            set
+            {
+                secAddress = value;
+            }
+        }
+        public string SecHome
+        {
+            get
+            {
+                return secHome;
+            }
+            set
+            {
+                secHome = value;
+            }
+        }
+
+        public string SecNotes
+        {
+            get
+            {
+                return secNotes;
+            }
+            set
+            {
+                secNotes = value;
+            }
+        }
+    }
+}
